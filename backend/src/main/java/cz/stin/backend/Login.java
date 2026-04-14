f46da566249a94ce9cf1cfe6a7afd80697b23d62
Matej Springl <matej.springl@tul.cz>
2026-04-14 18:34:23 +0200

    pridani frontendu ale jako static

---
+++ backend/src/main/java/cz/stin/backend/Login.java
@@ -0,0 +1,19 @@
+package cz.stin.backend;
+
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api")
+@CrossOrigin
+public class Login {
+
+    @PostMapping("/login")
+    public String login(@RequestBody String username) {
+
+        if (username != null && !username.isEmpty()) {
+            return "OK";
+        }
+
+        return "FAIL";
+    }
+}
